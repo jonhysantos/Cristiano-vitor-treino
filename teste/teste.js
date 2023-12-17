cc9c4bc0c2e22a77da8aada16fa3608203a3bbaf
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-16 23:26:02 -0300

    Faltam apenas os vídeos.
    Apenas os vídeos para disponibilizar o link hospedado

---
+++ teste/teste.js
@@ -0,0 +1,7 @@
+let body = document.getElementsByTagName('body')[0]
+let header = document.createElement('header')
+header.innerHTML = `<h1>Jonhy Alberto</h1>`
+header.innerHTML += `<p> Desenvolvedor Front-end</p>`
+body.appendChild(header)
+
+
