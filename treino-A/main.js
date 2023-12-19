737fe7b5076ea45727bc36bbb7da1b8643a05a48
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-19 00:07:27 -0300

    treino A completo
    Com interatividade e videos de checked.

--- treino-A/main.js
+++ treino-A/main.js
@@ -1,3 +1,4 @@
+// treino de peito interativo
 let supiReto = document.getElementsByTagName('video')[0]
 let checked = document.getElementsByTagName('input')[1]
 let crucBanco = document.getElementsByTagName('video')[1]
@@ -6,6 +7,17 @@ let crucMaq = document.getElementsByTagName('video')[2]
 let checked2 = document.getElementsByTagName('input')[3]
 let supMaq = document.getElementsByTagName('video')[3]
 let checked3 = document.getElementsByTagName('input')[4]
+
+// treino de triceps interativo
+let triBarra = document.getElementsByTagName('video')[4]
+let checked4 = document.getElementsByTagName('input')[5]
+let triCorda = document.getElementsByTagName('video')[5]
+let checked5 = document.getElementsByTagName('input')[6]
+let triFran = document.getElementsByTagName('video')[6]
+let checked6 = document.getElementsByTagName('input')[7]
+
+// treino de peito interativo
+
 checked.addEventListener('click',done)
 function done(){
     if(supiReto.style.display == 'none'){
@@ -51,4 +63,44 @@ function done3(){
     }
 
 }
+
+// treino de triceps interativo
+
+checked4.addEventListener('click',done4)
+function done4(){
+    if(triBarra.style.display == 'none'){
+        triBarra.style.display = 'block'
+        triBarra.style.margin = 'auto'
+    }else{
+      triBarra.style.display = 'none'  
+      checked4.style.marginTop = '15px' 
+    }
+
+}
+
+checked5.addEventListener('click',done5)
+function done5(){
+    if(triCorda.style.display == 'none'){
+        triCorda.style.display = 'block'
+        triCorda.style.margin = 'auto'
+    }else{
+      triCorda.style.display = 'none'  
+      checked5.style.marginTop = '15px' 
+    }
+
+}
+
+checked6.addEventListener('click',done6)
+function done6(){
+    if(triFran.style.display == 'none'){
+        triFran.style.display = 'block'
+        triFran.style.margin = 'auto'
+    }else{
+      triFran.style.display = 'none'  
+      checked6.style.marginTop = '15px' 
+    }
+
+}
+
+
 
