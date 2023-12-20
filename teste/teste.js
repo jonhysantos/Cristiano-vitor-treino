eefcde457fec8663b902100e74def3add3acc96e
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-19 23:33:32 -0300

    Treino B completo.

--- teste/teste.js
+++
@@ -1,7 +0,0 @@
-let body = document.getElementsByTagName('body')[0]
-let header = document.createElement('header')
-header.innerHTML = `<h1>Jonhy Alberto</h1>`
-header.innerHTML += `<p> Desenvolvedor Front-end</p>`
-body.appendChild(header)
-
-
